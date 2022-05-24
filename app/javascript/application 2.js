7db6d164d2da9325ea99c6db7ced5a2753c5f75b
Enow Mbi <benowmbi@yahoo.com>
2022-05-24 19:34:49 +0100

    Add javascripts

--- app/javascript/application 2.js
+++ app/javascript/application 2.js
@@ -1,13 +1,9 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
-
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
 document.observe("dom:loaded", function() {
-    $$('object').each(function(obj) {
-        a = document.createElement('param');
+    $$('object').each(function(obj){
+        a  = document.createElement('param');
         a.name = 'wmode';
         a.value = 'transparent';
         obj.appendChild(a);
